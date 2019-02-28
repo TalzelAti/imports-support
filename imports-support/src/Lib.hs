50dc3b5ee30667bd1ff69bad3a707e9b8d3c3bee
TalZ <tal.z@atidot.com>
2019-02-28 21:20:31 +0200

    added basic parser

--- imports-support/src/Lib.hs
+++ imports-support/src/Lib.hs
@@ -21,19 +21,186 @@ import           "text"                     Data.Text (Text)
 import           "filepath"            System.FilePath
 import                                Types
 --import           "base"                Data.Monoid
---import           "base"               System.IO
+--impodrt           "base"               System.IO
 --import           "extra"             Control.Monad.Extra
 import Control.Exception
+import Debug.Trace
 import System.IO.Error
+import qualified "parsec" Text.ParserCombinators.Parsec as P
+import qualified "parsec" Text.Parsec.Combinator as P
 
-removeIfExists :: FilePath -> IO ()
-removeIfExists fileName = removeFile fileName `catch` handleExists
-    where handleExists e
-            | isDoesNotExistError e = return ()
-            | otherwise = throwIO e
+
+import qualified "parsec" Text.ParserCombinators.Parsec.Expr as P
+import qualified "parsec" Text.ParserCombinators.Parsec.Language as P
+import qualified  "parsec" Text.ParserCombinators.Parsec.Token as Token
+
+data Qualified = QualDef | QualOnly  | QualAs ModuleName
+    deriving (Show)
+type ModuleName = [String]
+data ImportStmt =
+    ImportStmt
+        { _importStmtQualOnly_pkgImport  :: Maybe String
+        , _importStmtQualOnly_moduleName :: ModuleName
+        , _importStmtQualOnly_importExtra :: Maybe ImportExtra
+        , _importStmtQualOnly_qualified :: Qualified
+        }
+
+        deriving (Show)
+
+data ImportExtra = HidingList [String] | ImportList [String] | InstancesOnly
+    deriving (Show)
+
+languageDef =
+  P.emptyDef { Token.commentStart    = "{-"
+             , Token.commentEnd      = "-}"
+             , Token.commentLine     = "--"
+             , Token.identStart      = P.letter
+             , Token.identLetter     = P.alphaNum
+             , Token.reservedNames   = [ "import"
+                                       , "qualified"
+                                       , "as"
+                                       , "hiding"
+                                       ]
+
+             }
+
+lexer = Token.makeTokenParser languageDef
+
+identifier = Token.identifier lexer -- parses an identifier
+reserved   = Token.reserved   lexer -- parses a reserved name
+parens     = Token.parens     lexer -- parses surrounding parenthesis:
+                                    --   parens p
+                                    -- takes care of the parenthesis and
+                                    -- uses p to parse what's inside them
+comma      = Token.comma     lexer -- parses a semicolon
+whiteSpace = Token.whiteSpace lexer -- parses whitespace
+commaSep = Token.commaSep    lexer
+commaSep1 = Token.commaSep1    lexer
+symbol = Token.symbol    lexer
+
+whileParser :: P.Parser [ImportStmt]
+whileParser = do --P.sepBy statementP (whiteSpace >> P.char '\n' >> return ())
+    many statementP
+    where
+
+statementP :: P.Parser ImportStmt
+statementP = importStmtP
+        -- <|> importQualifiedOnlyP
+
+importStmtP :: P.Parser ImportStmt
+importStmtP = do
+    reserved "import"
+    whiteSpace
+    mPackageImport <- mPkgImportPrsr
+    whiteSpace
+    moduleName <- nameParser
+    whiteSpace
+    return $ ImportStmt
+        mPackageImport
+        moduleName
+        Nothing
+        QualDef
+
+mPkgImportPrsr :: P.Parser (Maybe String)
+mPkgImportPrsr = P.optionMaybe $ do
+    P.char '"'
+    name <- identifier
+    P.char '"'
+    return  name
+
+nameParser = P.sepBy1 identifier (P.char '.')
+
+        -- mAsPrsr = do
+        --     reserved "as"
+        --     QualAs <$> identifier
+        -- mImportListParser =
+        --     importsListParser <|> hidingListParser
+        -- importsListParser = do
+        --     idens  <- parens $ commaSep identifier
+        --     return $ if null idens
+        --              then InstancesOnly
+        --              else ImportList idens
+        -- hidingListParser = do
+        --     reserved "hiding"
+        --     idens  <- parens $ commaSep1 identifier
+        --     return $ if null idens
+        --              then InstancesOnly
+        --              else ImportList idens
+
+importQualifiedOnlyP :: P.Parser ImportStmt
+importQualifiedOnlyP = do undefined
+    -- reserved "import"
+    -- mQualified <- qualifiedTokenParser
+    -- whiteSpace
+    -- mPackageImport <- mPkgImportPrsr
+    -- moduleName <- nameParser
+    -- mAs <- mAsPrsr
+    --mImportList <- mImportListParser
+    -- let qual = f mPackageImport mAs
+    -- return $ lttrace "importStmt" $ ImportStmt
+    --     qual
+    --     mPackageImport
+    --     moduleName
+    --     Nothing
+    where
+        -- qualifiedTokenParser :: P.Parser Qualified
+        -- qualifiedTokenParser =
+        --     (reserved "qualified" >> whiteSpace >> return (QualOnly []))
+        --     <|> (whiteSpace >> return (QualAs []))
+        -- mPkgImportPrsr :: P.Parser (Maybe String)
+        -- mPkgImportPrsr = P.optionMaybe mPkgImportPrsr'
+        -- mPkgImportPrsr' = do
+        --     P.char '"'
+        --     name <- identifier
+        --     P.char '"'
+        --     return $ lttrace "just name" name
+
+        -- nameParser = do
+        --     whiteSpace
+        --     P.sepBy1 identifier (P.char '.')
+        -- mAsPrsr = do
+        --     reserved "as"
+        --     QualAs <$> identifier
+        -- mImportListParser =
+        --     importsListParser <|> hidingListParser
+        -- importsListParser = do
+        --     idens  <- parens $ commaSep identifier
+        --     return $ if null idens
+        --              then InstancesOnly
+        --              else ImportList idens
+        -- hidingListParser = do
+        --     reserved "hiding"
+        --     idens  <- parens $ commaSep1 identifier
+        --     return $ if null idens
+        --              then InstancesOnly
+        --              else ImportList idens
+
+parseString :: String -> [ImportStmt]
+parseString str =
+  case P.parse whileParser "" str of
+    Left e  -> error $ show e
+    Right r -> r
+
+
+
+-- bExpression :: Parser BExpr
+-- bExpression = buildExpressionParser bOperators bTerm
+
+
+-- bOperators = [ [Prefix (reserved "not" >> return (Not             ))          ]
+--              , [Infix  (reservedOp "and" >> return (BBinary And     )) AssocLeft,
+--                 Infix  (reservedOp "or"  >> return (BBinary Or      )) AssocLeft]
+--              ]
+
+
+
+-- bTerm =  parens bExpression
+--      <|> (reserved "true"  >> return (BoolConst True ))
+--      <|> (reserved "false" >> return (BoolConst False))
+--      <|> rExpression
 
 --import Debug.Trace
---lttrace a b = trace (a ++ ":" ++ show b) b
+lttrace a b = trace (a ++ ":" ++ show b) b
 
 -- todo: deal with comments
 -- todo: deal with multilines
@@ -43,9 +210,22 @@ removeIfExists fileName = removeFile fileName `catch` handleExists
 -- todo: importify file (add package imports and labels)
 -- todo: unique temp names
 
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+    where handleExists e
+            | isDoesNotExistError e = return ()
+            | otherwise = throwIO e
+
 someFunc :: IO ()
-someFunc = execParser opts
-    >>= runCmd
+someFunc = do
+    fileContent <- readFile "/home/talz/development/imports-support/test.hs"
+    putStrLn "########"
+    putStrLn fileContent
+    putStrLn "########"
+    putStrLn $ show fileContent
+    putStrLn "########"
+    pPrint $ parseString fileContent
+    -- execParser opts >>= runCmd
     where
         opts = info (options <**> helper)
             ( fullDesc
@@ -168,6 +348,12 @@ annotatePackage (Package packagePath _) = do
         hsFileContent <- readFile hsFile
         let importsList = findImportLines . lines $  hsFileContent -- optimize the readFile call
             hasPackageImports = any isPackageImports . lines $ hsFileContent
+        pPrint hsFile
+        pPrint ">>>>>>>>>>>>>>>>>>>>>>>>>"
+        pPrint $ importsList
+        putStrLn $ head importsList
+        pPrint $ parseString (head importsList)
+        pPrint "<<<<<<<<<<<<<<<<<<<<<<<<<"
         return $ HsFileAnnot hsFile hasPackageImports importsList
     let annot = case mPackageYamlFile of
             Just f  -> PackageAnnot (PkgsYaml f:fsAnns)
@@ -228,8 +414,12 @@ isPackageImports :: String -> Bool
 isPackageImports =  (["{-#","LANGUAGE","PackageImports","#-}"] ==) . words
 
 findImportLines :: [String] -> [String]
-findImportLines = takeWhile isImport . dropWhile (not . isImport) . filter (not . null)
-    where isImport = isPrefixOf "import"
+findImportLines = takeWhile isImportClause . dropWhile (not . isImport)
+    where isImport = isPrefixOf "import "
+          isLineComment = isPrefixOf "--" . filter (not . (' '==))
+          isMultiLineImport = isPrefixOf " "
+          isImportClause x = isImport x || null x || isMultiLineImport x || isLineComment x
+
 
 getPackageNames :: [String] -> [String]
 getPackageNames = nub . removeBaseModule . map takePackageDeclrs . filter hasPackageDclr
